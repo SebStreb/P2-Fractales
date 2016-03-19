e089f3755e3848b6dbff34ea6f9c51ca86bc0f9e
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2016-03-19 13:03:23 +0100

    Added template

---
+++ libfractal/fractal.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "fractal.h"
+
+struct fractal *fractal_new(int width, int height, double a, double b)
+{
+    /* TODO */
+    return NULL;
+}
+
+void fractal_free(struct fractal *f)
+{
+    /* TODO */
+}
+
+int fractal_get_value(const struct fractal *f, int x, int y)
+{
+    /* TODO */
+    return 0;
+}
+
+void fractal_set_value(struct fractal *f, int x, int y, int val)
+{
+    /* TODO */
+}
+
+int fractal_get_width(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
+
+int fractal_get_height(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
+
+double fractal_get_a(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
+
+double fractal_get_b(const struct fractal *f)
+{
+    /* TODO */
+    return 0;
+}
