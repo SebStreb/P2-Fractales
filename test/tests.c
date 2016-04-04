69f670c0249250a26ac49ed49fdaace36adf5f19
CelineDknp <celine.deknop@student.uclouvain.be>
2016-04-04 13:27:04 +0200

    Du coup, juste l'ajout d'un test et le reste marche parfaitement

--- test/tests.c
+++ test/tests.c
@@ -2,9 +2,21 @@
 #include <CUnit/basic.h>
 #include "../libfractal/fractal.h"
 
-void test1(void) {
-	struct fractal * test = fractal_new("Test", 100, 30, 0.5, 0.4);
-	CU_PASS("Test réussi !");
+void testFractales(void) {
+	char * name = "Test";
+	int hauteur =100;
+	int largeur = 50;
+	double a = 0.75;
+	double b = -0.7;
+	double moyenne = 2.221400;
+	struct fractal * test = fractal_new(name, hauteur, largeur, a, b);
+	test=fractal_fill(test);
+	CU_ASSERT_STRING_EQUAL(name, fractal_get_name(test));
+	CU_ASSERT_EQUAL(hauteur, fractal_get_height(test));
+	CU_ASSERT_EQUAL(largeur, fractal_get_width(test));
+	CU_ASSERT_EQUAL(a, fractal_get_a(test));
+	CU_ASSERT_EQUAL(b, fractal_get_b(test));
+	CU_ASSERT_EQUAL(moyenne, fractal_get_av(test));
 }
 
 int setup(void) {
@@ -26,7 +38,7 @@ int main(int argc, char const *argv[]) {
 		return CU_get_error();
 	}
 
-	if ((NULL == CU_add_test(pSuite, "test1", test1))
+	if ((NULL == CU_add_test(pSuite, "test de la librairie fractale", testFractales))
 	) {
 		CU_cleanup_registry();
 		return CU_get_error();
