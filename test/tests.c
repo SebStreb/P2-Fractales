3f1c67d0f71c7a916cc33d8f4cccd65cd472bb45
Tux <tux@localhost.localdomain>
2016-05-06 11:30:55 +0200

    some fix on tests

--- test/tests.c
+++ test/tests.c
@@ -8,6 +8,7 @@ void testGetSetValue(void) {
 	int x = 10;
 	fractal_set_value(test, 1, 1, x);
 	CU_ASSERT_EQUAL(x, fractal_get_value(test, 1, 1));
+	fractal_free(test);
 }
 
 void testGetSetAv(void) {
@@ -15,6 +16,7 @@ void testGetSetAv(void) {
 	int x = 10;
 	fractal_set_av(test, x);
 	CU_ASSERT_EQUAL(x, fractal_get_av(test));
+	fractal_free(test);
 }
 
 void testFractalesNames(void) {
@@ -26,11 +28,12 @@ void testFractalesNames(void) {
 	struct fractal * test = fractal_new(name, largeur, hauteur, a, b);
 	test=fractal_fill(test);
 	CU_ASSERT_STRING_EQUAL(name, fractal_get_name(test));
+	fractal_free(test);
 }
 
 void testFractalesSize(void) {
 	char * name = "Test";
-	int largeur =100;
+	int largeur = 100;
 	int hauteur = 50;
 	double a = 0.75;
 	double b = -0.7;
@@ -38,11 +41,12 @@ void testFractalesSize(void) {
 	test=fractal_fill(test);
 	CU_ASSERT_EQUAL(hauteur, fractal_get_height(test));
 	CU_ASSERT_EQUAL(largeur, fractal_get_width(test));
+	fractal_free(test);
 }
 
 void testFractalesAB(void) {
 	char * name = "Test";
-	int largeur =100;
+	int largeur = 100;
 	int hauteur = 50;
 	double a = 0.75;
 	double b = -0.7;
@@ -50,11 +54,12 @@ void testFractalesAB(void) {
 	test=fractal_fill(test);
 	CU_ASSERT_EQUAL(a, fractal_get_a(test));
 	CU_ASSERT_EQUAL(b, fractal_get_b(test));
+	fractal_free(test);
 }
 
 void testFractalesAverage(void) {
 	char * name = "Test";
-	int largeur =100;
+	int largeur = 100;
 	int hauteur = 50;
 	double a = 0.75;
 	double b = -0.7;
@@ -62,24 +67,29 @@ void testFractalesAverage(void) {
 	struct fractal * test = fractal_new(name, largeur, hauteur, a, b);
 	test=fractal_fill(test);
 	CU_ASSERT_EQUAL(moyenne, fractal_get_av(test));
+	fractal_free(test);
 }
 
 void testStackSize(void){
-	node * stackTest;
+	node * stackTest = malloc(sizeof(node));
 	int base = 0;
 	CU_ASSERT_EQUAL(base, stack_length(stackTest));
 	struct fractal * fill = fractal_new("Nom", 10, 10, 1, 0.6);
 	int plus = 1;
 	stack_push(&stackTest, fill);
 	CU_ASSERT_EQUAL(plus, stack_length(stackTest));
+	fractal_free(fill);
+	free(stackTest);
 }
 
 void testStackPushPop(void){
-	node * stackTest;
+	node * stackTest = malloc(sizeof(node));
 	struct fractal * fill = fractal_new("Nom", 10, 10, 1, 0.6);
 	stack_push(&stackTest, fill);
 	struct fractal * same = stack_pop(&stackTest);
 	CU_ASSERT_EQUAL(fill, same);
+	fractal_free(fill);
+	free(stackTest);
 }
 
 int setup(void) {
@@ -105,17 +115,17 @@ int main(int argc, char const *argv[]) {
 		NULL == CU_add_test(pSuite, "test get/set value", testGetSetValue) ||
 		NULL == CU_add_test(pSuite, "test get/set average", testGetSetAv) ||
 		NULL == CU_add_test(pSuite, "test de la librairie fractale, nom", testFractalesNames) ||
-		NULL == CU_add_test(pSuite, "test de la librairie fractale, taille", testFractalesSize ||
+		NULL == CU_add_test(pSuite, "test de la librairie fractale, taille", testFractalesSize) ||
 		NULL == CU_add_test(pSuite, "test de la librairie fractale, a et b", testFractalesAB) ||
 		NULL == CU_add_test(pSuite, "test de la librairie fractale, moyenne", testFractalesAverage) ||
 		NULL == CU_add_test(pSuite, "test de la librairie stack, taille", testStackSize) ||
 		NULL == CU_add_test(pSuite, "test de la librairie stack, push et pop", testStackSize)
-	)) {
+	) {
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
 
-	//CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_set_mode(CU_BRM_VERBOSE);
 	CU_basic_run_tests();
 	CU_basic_show_failures(CU_get_failure_list());
 	CU_cleanup_registry();
